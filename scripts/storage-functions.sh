f6cf72a3abd8a770ecfe33920dc3d8a0ca7af395
Kuba <37076321+kubaseai@users.noreply.github.com>
2019-01-22 15:35:15 +0100

    Update storage-functions.sh
    not all systems use btrfs root

--- scripts/storage-functions.sh
+++ scripts/storage-functions.sh
@@ -1,14 +1,14 @@
 function setupStorage {
     echo "setupStorage"
     mkdir -p /mnt/local-storage 2>/dev/null
-    for dir in "pgsql-data"; do
-        mkdir -p /mnt/local-storage/$dir
-        btrfs subvolume create "/$dir" 2>/dev/null
-        ID=`btrfs subvolume list / | grep $dir | awk '{print $2}'`
-        DEVICE=`mount | grep root | head -n 1 | awk '{print $1}'`
-        MOUNTED=`mount | grep -c $dir`
-        [[ $MOUNTED -gt 0 ]] || mount $DEVICE /mnt/local-storage/$dir -o subvolid=$ID 2>/dev/null
-    done
+#    for dir in "pgsql-data"; do
+#        mkdir -p /mnt/local-storage/$dir
+#        btrfs subvolume create "/$dir" 2>/dev/null
+#        ID=`btrfs subvolume list / | grep $dir | awk '{print $2}'`
+#        DEVICE=`mount | grep root | head -n 1 | awk '{print $1}'`
+#        MOUNTED=`mount | grep -c $dir`
+#        [[ $MOUNTED -gt 0 ]] || mount $DEVICE /mnt/local-storage/$dir -o subvolid=$ID 2>/dev/null
+#    done
     chmod a+w /mnt/local-storage
     chmod a+r /mnt/local-storage
     chcon -R unconfined_u:object_r:svirt_sandbox_file_t:s0 /mnt/local-storage/
